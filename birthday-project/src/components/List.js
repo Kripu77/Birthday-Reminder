18e016e038fdabeaa73636ccdfd0f4dfa5274fa2
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-27 15:53:43 +1000

    image links

---
+++ birthday-project/src/components/List.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+import {data} from "../data";
+const List = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default List
---
+++ birthday-project/src/data.js
@@ -0,0 +1,14 @@
+export const data = [
+    {
+id:1, name:"Pushpa Khadka", age:38, image:"https://www.facebook.com/photo/?fbid=564363950771320&set=a.110375402836846"
+    },{
+id:2, name: "Krishna Khadka", age:47, image:"https://www.facebook.com/photo.php?fbid=551448419128035&set=pb.100027886461871.-2207520000..&type=3"
+    },{
+id: 3, name: "Kimod Thapa", age:23, image:"https://www.facebook.com/photo/?fbid=3727963433952242&set=a.100399740041981"
+    },{
+        id:4, name:"Rajesh Thapa", age: 25, image:"https://www.facebook.com/photo.php?fbid=1573047136178683&set=pb.100004202350839.-2207520000..&type"
+
+    },{
+id:5, name: "Samman Malla", age:22, image:"https://www.facebook.com/photo.php?fbid=608712102802004&set=t.100003009043657&type=3"
+    }
+]
